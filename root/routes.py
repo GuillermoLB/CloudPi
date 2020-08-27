05f410a49bd2f30eeb3dd283e09c39d698615910
Guillermo <guillelb99@gmail.com>
2020-08-27 21:56:34 +0200

    Added bootstrap.

--- root/routes.py
+++ root/routes.py
@@ -1,4 +1,5 @@
 from root import app
+import os #para ejecutar comanmdos linux
 from flask import render_template, request
 
 @app.route('/')
@@ -8,8 +9,10 @@ def index():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    selectedFile = request.files['selectedFile'] #variable fileStorage
+    selectedFile = request.files['selectedFile'] #variable fileStorage recibida del form 'selectedFile' (en index.html)
     selectedFile_name = selectedFile.filename
     selectedFile_route = 'root/static/uploads/{}'.format(selectedFile_name) #probar con ruta absoluta de modo
     selectedFile.save(selectedFile_route)
+    os.system('cd root/static/uploads && mkdir dir5')
+    
     return selectedFile.filename
